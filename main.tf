d995d5ff901bcdaf6741510dd83109090c7158ea
Ukie555 <309538567@qq.com>
2024-11-05 14:23:06 -0800

    update loop

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "google_compute_instance" "nginx_instance" {
     environment = var.environment_map[var.target_environment]
   }
   tags = var.compute-source-tags
-  
+ 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
@@ -60,48 +60,9 @@ resource "google_compute_instance" "nginx_instance" {
   }
 }
 
-## WEB1
-resource "google_compute_instance" "web1" {
-  name         = "web1"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    # A default network is created for all GCP projects
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB2
-resource "google_compute_instance" "web2" {
-  name         = "web2"
-  machine_type = var.environment_machine_type[var.target_environment]
-  labels = {
-    environment = var.environment_map[var.target_environment]
-  }
-  
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = data.google_compute_network.default.self_link
-    subnetwork = google_compute_subnetwork.subnet-1.self_link
-  }
-}
-## WEB3
-resource "google_compute_instance" "web3" {
-  name         = "web3"
+resource "google_compute_instance" "web-instances" {
+  count = 3
+  name         = "web${count.index}"
   machine_type = var.environment_machine_type[var.target_environment]
   labels = {
     environment = var.environment_map[var.target_environment]
@@ -118,6 +79,65 @@ resource "google_compute_instance" "web3" {
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
 }
+
+# WEB1
+# resource "google_compute_instance" "web1" {
+#   name         = "web1"
+#   machine_type = var.environment_machine_type[var.target_environment]
+#   labels = {
+#     environment = var.environment_map[var.target_environment]
+#   }
+  
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = data.google_compute_network.default.self_link
+#     subnetwork = google_compute_subnetwork.subnet-1.self_link
+#   }
+# }
+# ## WEB2
+# resource "google_compute_instance" "web2" {
+#   name         = "web2"
+#   machine_type = var.environment_machine_type[var.target_environment]
+#   labels = {
+#     environment = var.environment_map[var.target_environment]
+#   }
+  
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+
+#   network_interface {
+#     network = data.google_compute_network.default.self_link
+#     subnetwork = google_compute_subnetwork.subnet-1.self_link
+#   }
+# }
+# ## WEB3
+# resource "google_compute_instance" "web3" {
+#   name         = "web3"
+#   machine_type = var.environment_machine_type[var.target_environment]
+#   labels = {
+#     environment = var.environment_map[var.target_environment]
+#   }
+  
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-11"
+#     }
+#   }
+
+#   network_interface {
+#     network = data.google_compute_network.default.self_link
+#     subnetwork = google_compute_subnetwork.subnet-1.self_link
+#   }  
+# }
 
 ## DB
 resource "google_compute_instance" "mysqldb" {
@@ -137,4 +157,27 @@ resource "google_compute_instance" "mysqldb" {
     network = data.google_compute_network.default.self_link
     subnetwork = google_compute_subnetwork.subnet-1.self_link
   }  
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+## CLOUD SQL
+resource "google_sql_database_instance" "cloudsql" {
+  name             = "web-app-db-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_8_0"
+  region           = "us-central1"
+
+  settings {
+    tier = "db-f1-micro"
+  }
+  deletion_protection = false
+}
+
+## CLOUD SQL USER
+resource "google_sql_user" "users" {
+  name     = var.dbusername
+  instance = google_sql_database_instance.cloudsql.name
+  password = var.dbpassword
 }
